d308af1f33d8a4046d693cbb401195d5d0f47108
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro.local>
2023-10-31 11:13:57 -0500

    begin exercises

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -20,9 +20,24 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  const user = userEvent.setup();
+  render(<Counter initialCount={100} />);
 
-test.todo(
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('100');
+});
+
+test(
   'it should reset the count when the "Reset" button is pressed',
-  async () => {},
+  async () => {
+    const user = userEvent.setup();
+    render(<Counter initialCount={6} />);
+
+    const currentCount = screen.getByTestId('current-count');
+    const resetButton = screen.getByRole('button', { name: 'Reset' });
+
+    await user.click(resetButton);
+    expect(currentCount).toHaveTextContent('0');
+  },
 );
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,5 +1,16 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { render as _render, screen } from 'test/utilities';
+import { PackingList } from '.';
+import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { createStore } from './store';
+
+function render(ui: React.ReactElement) {
+  return _render(
+    <Provider store={createStore()}>
+      <PackingList />
+    </Provider>
+  )
+}
 
 it('renders the Packing List application', () => {
   render(<PackingList />);
@@ -10,19 +21,48 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+  render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.findByRole('button', { name: 'Add New Item' });
+
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled;
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+    const user = userEvent.setup();
+    render(<PackingList />);
+    const newItemInput = screen.getByLabelText('New Item Name');
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
+  
+    await user.type(newItemInput, 'hi');
+
+    expect(addNewItemButton).toBeEnabled;
+  },
 );
 
-it.todo(
+it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
+  async () => {
+    const user = userEvent.setup();
+    render(<PackingList />);
+    const newItemInput = screen.getByLabelText('New Item Name');
+    const addNewItemButton = screen.getByRole('button', { name: 'Add New Item' });
+  
+    await user.type(newItemInput, 'hi');
+    await user.click(addNewItemButton);
+
+    expect(screen.getByLabelText('hi')).not.toBeChecked();
+  },
 );
